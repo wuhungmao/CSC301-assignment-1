03b6bc3c63ddaa506385df6e6ed06ed1957539ba
choijo18 <jonathanc.choi@mail.utoronto.ca>
2024-03-12 19:59:57 -0400

    testing backup docker

--- a2/docker.sh
+++ a2/docker.sh
@@ -10,15 +10,19 @@ docker run -d \
     postgres
 
 # Wait for PostgreSQL to initialize
-sleep 10
+sleep 9
 
 docker exec -it postgres-container sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/postgresql/data/postgresql.conf
 
 docker exec -it postgres-container service postgresql restart
 
-# Create databases
-docker exec -it postgres-container psql -U postgres -c "CREATE DATABASE Users;"
-docker exec -it postgres-container psql -U postgres -c "CREATE DATABASE Product"
-docker exec -it postgres-container psql -U postgres -c "CREATE DATABASE Orders;"
+docker exec -it postgres-container psql -U postgres -c "CREATE DATABASE users;"
+docker exec -it postgres-container psql -U postgres -c "CREATE DATABASE product;"
+docker exec -it postgres-container psql -U postgres -c "CREATE DATABASE orders;"
 
-echo "PostgreSQL setup complete."
+sleep 5
+
+docker exec -it postgres-container psql -U postgres -d users -c "CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, username TEXT NOT NULL, email TEXT NOT NULL UNIQUE, password TEXT NOT NULL);"
+docker exec -it postgres-container psql -U postgres -d product -c "CREATE TABLE products (productId INTEGER PRIMARY KEY, productName TEXT NOT NULL, description TEXT NOT NULL, price DOUBLE PRECISION NOT NULL, quantity INTEGER NOT NULL);"
+docker exec -it postgres-container psql -U postgres -d orders -c "CREATE TABLE IF NOT EXISTS orders (orderId SERIAL PRIMARY KEY, userId INTEGER NOT NULL, productId INTEGER NOT NULL, quantity INTEGER NOT NULL);"
+
